797c40b1709c421866c435372897e6ff7846f279
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-09-03 00:45:40 +0300

    final

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -2,7 +2,7 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSER_APP_ID!,
+  appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: "eu",
